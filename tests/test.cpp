25c7b908a831ce287e308df6232777d2c7d37895
justddreamm <lagovsp@gmail.com>
2022-04-15 23:16:44 +0300

    GTEST set up

---
+++ tests/test.cpp
@@ -0,0 +1,11 @@
+// Copyright 2022 Sergey Lagov lagovsp@gmail.com
+
+#include "ram.h"
+#include <gtest/gtest.h>
+
+using namespace std;
+using namespace RAM;
+
+TEST(RAM, Simple) {
+  EXPECT_TRUE(true);
+}
