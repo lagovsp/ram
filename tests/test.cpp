4a087a8531e6b37e25562758588799a8791ca826
justddreamm <lagovsp@gmail.com>
2022-04-15 23:32:12 +0300

    piece of tests added

--- tests/test.cpp
+++ tests/test.cpp
@@ -6,6 +6,14 @@
 using namespace std;
 using namespace RAM;
 
-TEST(RAM, Simple) {
-  EXPECT_TRUE(true);
+TEST(RAM, task_pow3n_plus_pow2n) {
+  Machine m;
+  ofstream os;
+  ifstream ss("../tasks/3**n+2**n/source.txt");
+
+  m.set_log_stream(os);
+  m.set_code(ss);
+  m.set_input({15});
+  m.run();
+  ASSERT_EQ(14381675, m.output().front());
 }
